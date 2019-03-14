05ec7d5afa4657b1db27644f5755fe93c517b1f8
ekaterina-nikonova <ekaterina.v.nikonova@gmail.com>
2019-03-14 01:04:23 +0100

    Remove path scheme from urls in api service

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
 
-export const createBoard = data => axios.post('http://localhost:3001/api/v1/boards', data);
+export const createBoard = data => axios.post('/api/v1/boards', data);
 
-export const deleteBoard = id => axios.delete(`http://localhost:3001/api/v1/boards/${id}`);
+export const deleteBoard = id => axios.delete(`/api/v1/boards/${id}`);
 
-export const getBoards = () => axios.get('http://localhost:3001/api/v1/boards.json');
+export const getBoards = () => axios.get('/api/v1/boards.json');
 
-export const getBoard = id => axios.get(`http://localhost:3001/api/v1/boards/${id}`);
+export const getBoard = id => axios.get(`/api/v1/boards/${id}`);
 
-export const updateBoard = ({ boardId, updates }) => axios.patch(`http://localhost:3001/api/v1/boards/${boardId}`, updates);
+export const updateBoard = ({ boardId, updates }) => axios.patch(`/api/v1/boards/${boardId}`, updates);
 
 /** Components */
 
-export const getComponents = boardId => axios.get(`http://localhost:3001/api/v1/boards/${boardId}/components`);
+export const getComponents = boardId => axios.get(`/api/v1/boards/${boardId}/components`);
 
-export const updateComponent = ({ componentId, updates }) => axios.patch(`http://localhost:3001/api/v1/components/${componentId}`, updates);
+export const updateComponent = ({ componentId, updates }) => axios.patch(`/api/v1/components/${componentId}`, updates);
