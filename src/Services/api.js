f8d8030bb4b9654924e6bc35cf8becaf74d15a8f
ekaterina-nikonova <ekaterina.v.nikonova@gmail.com>
2019-03-14 14:45:55 +0100

    Use environment-based base URL for api calls

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,17 +1,21 @@
 import axios from 'axios';
 
-export const createBoard = data => axios.post('/api/v1/boards', data);
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://make-sense-api.herokuapp.com' : '';
 
-export const deleteBoard = id => axios.delete(`/api/v1/boards/${id}`);
+console.log(`Base URL ${baseUrl} for ${process.env.NODE_ENV} environment`);
 
-export const getBoards = () => axios.get('/api/v1/boards.json');
+export const createBoard = data => axios.post(`${baseUrl}/api/v1/boards`, data);
 
-export const getBoard = id => axios.get(`/api/v1/boards/${id}`);
+export const deleteBoard = id => axios.delete(`${baseUrl}/api/v1/boards/${id}`);
 
-export const updateBoard = ({ boardId, updates }) => axios.patch(`/api/v1/boards/${boardId}`, updates);
+export const getBoards = () => axios.get(`${baseUrl}/api/v1/boards.json`);
+
+export const getBoard = id => axios.get(`${baseUrl}/api/v1/boards/${id}`);
+
+export const updateBoard = ({ boardId, updates }) => axios.patch(`${baseUrl}/api/v1/boards/${boardId}`, updates);
 
 /** Components */
 
-export const getComponents = boardId => axios.get(`/api/v1/boards/${boardId}/components`);
+export const getComponents = boardId => axios.get(`${baseUrl}/api/v1/boards/${boardId}/components`);
 
-export const updateComponent = ({ componentId, updates }) => axios.patch(`/api/v1/components/${componentId}`, updates);
+export const updateComponent = ({ componentId, updates }) => axios.patch(`${baseUrl}/api/v1/components/${componentId}`, updates);
