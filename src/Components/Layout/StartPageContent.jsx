84cd4c97a818958a1433a36f6ad08f164b485604
ekaterina <ekaterina.v.nikonova@gmail.com>
2019-10-05 21:25:53 +0200

    Redirect to Boards page on successful login

--- src/Components/Layout/StartPageContent.jsx
+++ src/Components/Layout/StartPageContent.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
+
+import { LoggedInContext } from "../../App";
 
 const StartPageContent = () => (
-  <div>Start page</div>
+  <LoggedInContext.Consumer>
+    {loggedIn => (
+      loggedIn ? <Redirect to='/boards' /> : <div>Start</div>
+    )}
+  </LoggedInContext.Consumer>
 );
 
 export default StartPageContent;
