7b70a1e3629a9a2ed3dc71995958092b63f58eb1
ekaterina <ekaterina.v.nikonova@gmail.com>
2019-10-10 02:18:43 +0200

    Add tooltip with a disclaimer about test mode to the invitation form

--- src/Components/Layout/StartPage/HeroSection.jsx
+++ src/Components/Layout/StartPage/HeroSection.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Typography } from "antd";
+import { Icon, Tooltip, Typography } from "antd";
 
 import InvitationRequestForm from "./InvitationRequestForm";
 
@@ -12,6 +12,11 @@ const HeroSection = () => {
       <Title>Keep 'em pins in order</Title>
       <Paragraph className="subtitle">
         To sign up, you'll need an invitation code.
+        <Tooltip
+          title="The service is working in a closed testing mode, and is not yet open for the general public."
+        >
+          <Icon type="info-circle" style={{ paddingLeft: "1rem" }} />
+        </Tooltip>
       </Paragraph>
       <InvitationRequestForm />
     </Typography>
