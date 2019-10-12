af40d7dbf7ee6d729b7d4a846892d7a7c78b2444
ekaterina <ekaterina.v.nikonova@gmail.com>
2019-10-12 23:36:59 +0200

    Hide delete icon for current user in the user list

--- src/Components/AdminPanel/UsersContainer.jsx
+++ src/Components/AdminPanel/UsersContainer.jsx
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useGlobal } from "reactn";
 
 import { deleteUser, getUsers } from "../../Services/api";
+import { UserContext } from "../../App";
 
 import { Alert, Icon, List, Popconfirm } from "antd";
+
 import EmptyFullPage from "../UI/EmptyFullPage";
 
 const UsersContainer = () => {
@@ -32,28 +34,34 @@ const UsersContainer = () => {
 
       {(!error && !users.length) && <EmptyFullPage />}
 
-      {(!error && users.length) && (<List
-        dataSource={users}
-        renderItem={user => (
-          <List.Item
-            actions={[
-              <Popconfirm
-                placement="topRight"
-                title={`Delete user ${user.email}?`}
-                onConfirm={() => handleDelete(user)}
-                okText="Yes"
-                cancelText="Cancel"
-              >
-                <Icon type="delete" />
-              </Popconfirm>
-            ]}
-          >
-            <List.Item.Meta
-              title={<a href={`profile/${user.id}`}>{user.email}</a>}
+      {(!error && users.length) && (
+        <UserContext.Consumer>
+          {currentUser => (
+            <List
+              dataSource={users}
+              renderItem={user => (
+                <List.Item
+                  actions={[
+                    user.id !== currentUser.id && <Popconfirm
+                      placement="topRight"
+                      title={`Delete user ${user.email}?`}
+                      onConfirm={() => handleDelete(user)}
+                      okText="Yes"
+                      cancelText="Cancel"
+                    >
+                      <Icon type="delete" />
+                    </Popconfirm>
+                  ]}
+                >
+                  <List.Item.Meta
+                    title={<a href={`profile/${user.id}`}>{user.email}</a>}
+                  />
+                </List.Item>
+              )}
             />
-          </List.Item>
-        )}
-      />)}
+          )}
+        </UserContext.Consumer>
+      )}
     </React.Fragment>
   );
 };
