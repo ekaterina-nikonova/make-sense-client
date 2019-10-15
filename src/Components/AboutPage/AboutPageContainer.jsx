cf7111be5758bfac5881e57d5e589fe4eb642e46
ekaterina <ekaterina.v.nikonova@gmail.com>
2019-10-15 23:52:23 +0200

    Write description and internals for Brittle Eye on the About page

--- src/Components/AboutPage/AboutPageContainer.jsx
+++ src/Components/AboutPage/AboutPageContainer.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-import { Divider, Icon, List, Timeline, Typography } from "antd";
+import { Divider, Timeline, Typography } from "antd";
 
 import TimelineSection from "./TimelineSection";
 
 const AboutPageContainer = () => {
-  const { Paragraph, Text, Title } = Typography;
+  const { Paragraph, Title } = Typography;
 
   const links = {
     androidX: 'https://developer.android.com/jetpack/androidx',
@@ -18,11 +18,27 @@ const AboutPageContainer = () => {
     rails: 'https://rubyonrails.org/'
   };
 
-  const brittleEyeLibraries = [];
   const brittleEyeDescription = (
     <React.Fragment>
+      A live image recognition app that allows to determine what the camera is pointed at.
+      Currently works with a small set of electronic components.
+      The app is written in <strong>Kotlin</strong>.
     </React.Fragment>
   );
+
+  const brittleEyeLibraries = [{
+    title: 'CameraX',
+    description: 'Get preview from device camera; handle preview rotation based on the data from device sensor',
+    link: 'https://developer.android.com/training/camerax'
+  }, {
+    title: 'Firebase Vision',
+    description: 'https://firebase.google.com/docs/reference/android/com/google/firebase/ml/vision/FirebaseVision',
+    link: 'Detect the most prominent object in the preview, track it\'s position and dimensions'
+  }, {
+    title: 'Firebase AutoML',
+    description: 'Recognise the object in the preview using a cloud-hosted re-trained model',
+    link: 'https://firebase.google.com/docs/ml-kit/automl-image-labeling'
+  }];
 
   const brittleUpDescription = (
     <React.Fragment>
@@ -55,7 +71,7 @@ const AboutPageContainer = () => {
     <React.Fragment>
       The Android client has the same features as the web app.
       It incorporates a few <a href={links.androidX} target="_blank" rel="noopener noreferrer">AndroidX</a> packages and manages user sessions internally.
-      The app is still under development.
+      <strong>The app is still under development</strong>.
     </React.Fragment>
   );
 
@@ -143,7 +159,7 @@ const AboutPageContainer = () => {
           <Timeline>
             <TimelineSection
               description={brittleEyeDescription}
-              icon={AndroidIcon}
+              icon={KotlinIcon}
               libraries={brittleEyeLibraries}
               title="Brittle Eye"
               subtitle="started Sep 29, 2019"
@@ -151,7 +167,7 @@ const AboutPageContainer = () => {
 
             <TimelineSection
               description={brittleUpDescription}
-              icon={JavaIcon}
+              icon={AndroidIcon}
               libraries={brittleUpLibraries}
               title="Brittle Up"
               subtitle="started Jun 1, 2019"
@@ -224,6 +240,24 @@ const JavaIcon = () => (
           d="M277.74 312.9c9.8-6.7 23.4-12.5 23.4-12.5s-38.7 7-77.2 10.2c-47.1 3.9-97.7 4.7-123.1 1.3-60.1-8 33-30.1 33-30.1s-36.1-2.4-80.6 19c-52.5 25.4 130 37 224.5 12.1zm-85.4-32.1c-19-42.7-83.1-80.2 0-145.8C296 53.2 242.84 0 242.84 0c21.5 84.5-75.6 110.1-110.7 162.6-23.9 35.9 11.7 74.4 60.2 118.2zm114.6-176.2c.1 0-175.2 43.8-91.5 140.2 24.7 28.4-6.5 54-6.5 54s62.7-32.4 33.9-72.9c-26.9-37.8-47.5-56.6 64.1-121.3zm-6.1 270.5a12.19 12.19 0 0 1-2 2.6c128.3-33.7 81.1-118.9 19.8-97.3a17.33 17.33 0 0 0-8.2 6.3 70.45 70.45 0 0 1 11-3c31-6.5 75.5 41.5-20.6 91.4zM348 437.4s14.5 11.9-15.9 21.2c-57.9 17.5-240.8 22.8-291.6.7-18.3-7.9 16-19 26.8-21.3 11.2-2.4 17.7-2 17.7-2-20.3-14.3-131.3 28.1-56.4 40.2C232.84 509.4 401 461.3 348 437.4zM124.44 396c-78.7 22 47.9 67.4 148.1 24.5a185.89 185.89 0 0 1-28.2-13.8c-44.7 8.5-65.4 9.1-106 4.5-33.5-3.8-13.9-15.2-13.9-15.2zm179.8 97.2c-78.7 14.8-175.8 13.1-233.3 3.6 0-.1 11.8 9.7 72.4 13.6 92.2 5.9 233.8-3.3 237.1-46.9 0 0-6.4 16.5-76.2 29.7zM260.64 353c-59.2 11.4-93.5 11.1-136.8 6.6-33.5-3.5-11.6-19.7-11.6-19.7-86.8 28.8 48.2 61.4 169.5 25.9a60.37 60.37 0 0 1-21.1-12.8z">
     </path>
   </svg>
+);
+
+const KotlinIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1.2em"
+    height="1.2em"
+    viewBox="0 0 60 60"
+    id="kotlin"
+  >
+    <linearGradient id="kotlin_a" gradientUnits="userSpaceOnUse" x1="15.959" y1="-13.014" x2="44.307" y2="15.333" gradientTransform="matrix(1 0 0 -1 0 61)"><stop offset=".097" stop-color="#0095D5" /><stop offset=".301" stop-color="#238AD9" /><stop offset=".621" stop-color="#557BDE" /><stop offset=".864" stop-color="#7472E2" /><stop offset="1" stop-color="#806EE3" /></linearGradient>
+    <path fill="url(#kotlin_a)" d="M0 60L30.1 29.9 60 60z" />
+
+    <linearGradient id="kotlin_b" gradientUnits="userSpaceOnUse" x1="4.209" y1="48.941" x2="20.673" y2="65.405" gradientTransform="matrix(1 0 0 -1 0 61)"><stop offset=".118" stop-color="#0095D5" /><stop offset=".418" stop-color="#3C83DC" /><stop offset=".696" stop-color="#6D74E1" /><stop offset=".833" stop-color="#806EE3" /></linearGradient>
+    <path fill="url(#kotlin_b)" d="M0 0L30.1 0 0 32.5z" />
+
+    <linearGradient id="kotlin_c" gradientUnits="userSpaceOnUse" x1="-10.102" y1="5.836" x2="45.731" y2="61.669" gradientTransform="matrix(1 0 0 -1 0 61)"><stop offset=".107" stop-color="#C757BC" /><stop offset=".214" stop-color="#D0609A" /><stop offset=".425" stop-color="#E1725C" /><stop offset=".605" stop-color="#EE7E2F" /><stop offset=".743" stop-color="#F58613" /><stop offset=".823" stop-color="#F88909" /></linearGradient>
+    <path fill="url(#kotlin_c)" d="M30.1 0L0 31.7 0 60 30.1 29.9 60 0z" /></svg>
 );
 
 const ReactIcon = () => (
