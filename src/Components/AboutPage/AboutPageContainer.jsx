4197bc49bbcbac8efaf6042e4934d661a911b6b2
ekaterina <ekaterina.v.nikonova@gmail.com>
2019-10-15 16:27:14 +0200

    Write intro for About page, create stylesheet

--- src/Components/AboutPage/AboutPageContainer.jsx
+++ src/Components/AboutPage/AboutPageContainer.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 
+import { Divider, Typography } from "antd";
+
 const AboutPageContainer = () => {
+  const { Paragraph, Title } = Typography;
+
   return (
-    <div>About page container</div>
+    <div className="about-container">
+      <div className="about-section">
+        <Divider>
+          <Title>About Brittle Pins</Title>
+        </Divider>
+
+        <Paragraph>
+          This project is a playground where I experiment with technologies that catch my eye.
+          It was conceived as an IoT project management app&nbsp;&mdash; a single place
+          to keep track of your equipment and code.
+        </Paragraph>
+
+        <Paragraph>
+          The core of Brittle Pins is an API built with <strong>Ruby on Rails</strong> and a web client built with <strong>React</strong>.
+          The accompanying Android apps are written in <strong>Kotlin</strong>, but some older code is still in <strong>Java</strong>.
+        </Paragraph>
+      </div>
+    </div>
   );
 };
 
