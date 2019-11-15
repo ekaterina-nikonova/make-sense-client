308e34a96ae0c536905b605492f111870ed7bb74
ekaterina <ekaterina.v.nikonova@gmail.com>
2019-11-15 04:45:23 +0100

    Set up GraphQL; show list of project names on section load

--- src/Services/graphql.js
+++ src/Services/graphql.js
@@ -2,31 +2,19 @@ import React from "react";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
-import { ApolloLink } from "apollo-link";
-import { onError } from "apollo-link-error";
-import { HttpLink } from "apollo-link-http";
+import { createHttpLink } from "apollo-link-http";
 import { gql } from "apollo-boost";
 
 import { baseUrl } from "./api";
 
-const httpLink = new HttpLink({
-  uri: `${baseUrl}/graphql`,
-  credentials: 'include'
+const httpLink = createHttpLink({
+  uri: `${baseUrl}/api/v1/graphql`,
+  credentials: 'include',
+  headers: { 'X-CSRF-TOKEN': localStorage.csrf }
 });
 
-const client = new ApolloClient({
-  link: ApolloLink.from([
-    onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors)
-        graphQLErrors.forEach(({ message, locations, path }) =>
-          console.log(
-            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-          ),
-        );
-      if (networkError) console.log(`[Network error]: ${networkError}`);
-    }),
-    httpLink
-  ]),
+export const client = new ApolloClient({
+  link: httpLink,
   cache: new InMemoryCache()
 });
 
