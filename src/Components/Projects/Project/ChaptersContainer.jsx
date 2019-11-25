9c52f09e874f02c09a5f2c8b7d5b2d5c78232aa9
ekaterina <ekaterina.v.nikonova@gmail.com>
2019-11-25 19:21:25 +0100

    Create UI for adding the first chapter in a project

--- src/Components/Projects/Project/ChaptersContainer.jsx
+++ src/Components/Projects/Project/ChaptersContainer.jsx
@@ -1,21 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Button, Empty, Tabs } from "antd";
+import { Button, Empty, Input, Tabs, Typography } from "antd";
 
 import Chapter from "./Chapter";
 
 const ChaptersContainer = ({ chapters, mobileScreen }) => {
+  const [ firstChapterEdit, setFirstChapterEdit ] = useState(false);
+  const [ firstChapterTitle, setFirstChapterTitle ] = useState('');
+  const [ firstChapterIntro, setFirstChapterIntro ] = useState('');
+
+  const { TextArea } = Input;
   const { TabPane } = Tabs;
+  const { Title } = Typography;
+
+  const toggleFirstChapterEdit = () =>
+    setFirstChapterEdit(!firstChapterEdit);
+
+  const createFirstChapter = () => {};
 
   return (
     <React.Fragment>
-      {(!chapters || chapters.length === 0) && (
+      {(!chapters || !chapters.length) && !firstChapterEdit && (
         <Empty
           image={Empty.PRESENTED_IMAGE_SIMPLE}
           description={false}
         >
-          <Button>Add a chapter</Button>
+          <Button onClick={toggleFirstChapterEdit}>
+            Add a chapter
+          </Button>
         </Empty>
+      )}
+
+      {(!chapters || !chapters.length) && firstChapterEdit && (
+        <div className="create-first-chapter-container">
+          <Title level={4}>New chapter</Title>
+          <Input
+            placeholder="Title"
+            onChange={e => setFirstChapterTitle(e.target.value)}
+          />
+          <TextArea
+            rows={5}
+            placeholder="Intro"
+            onChange={e => setFirstChapterIntro(e.target.value)}
+          />
+          <div>
+            <Button onClick={toggleFirstChapterEdit}>
+              Cancel
+            </Button>
+
+            <Button type="primary" onClick={createFirstChapter}>
+              Save
+            </Button>
+          </div>
+        </div>
       )}
 
       { chapters && (
