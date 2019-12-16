cd9223f4b26e0c85b1a28ba66e986933336d60c9
ekaterina <ekaterina.v.nikonova@gmail.com>
2019-12-16 16:39:54 +0100

    Add loading and error states to projects container

--- src/Components/Projects/ProjectsContainer.jsx
+++ src/Components/Projects/ProjectsContainer.jsx
@@ -10,7 +10,7 @@ import TopLevelMenu from "../Layout/TopLevelMenu";
 import ProjectList from "./ProjectList";
 import NewProjectDrawer from "./NewProjectDrawer";
 import ProjectWrapper from "./Project/ProjectWrapper";
-import { Empty } from "antd";
+import { Empty, Result, Spin } from "antd";
 
 const ProjectsContainer = ({ location, match }) => {
   const { pathname } = location;
@@ -22,8 +22,21 @@ const ProjectsContainer = ({ location, match }) => {
 
         <Query query={queries.projects}>
           {({ loading, error, data, subscribeToMore }) => {
-            if (loading) return <div>Loading...</div>;
-            if (error) return <div>Error :-(</div>;
+            if (loading) return (
+              <div className="top-level-state">
+                <Spin />
+              </div>
+            );
+
+            if (error) return (
+              <div className="top-level-state">
+                <Result
+                  status="error"
+                  title="Something's wrong"
+                  subTitle={error.message}
+                />
+              </div>
+            );
 
             if (!data || !data.projects || !data.projects.length) return (
               <Empty
